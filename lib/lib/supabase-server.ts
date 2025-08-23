0b04c1710732b15e838d616099f5daeab2c2d71c
tdfranklin101-ui <tdfranklin101@gmail.com>
2025-08-23 14:28:22 -0700

    Update supabase-server.ts

--- lib/lib/supabase-server.ts
+++ lib/lib/supabase-server.ts
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+export function supabaseService() {
+  return createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!, {
+    auth: { persistSession: false }
+  });
+}
 
